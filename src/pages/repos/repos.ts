c785a2f912f7690ac4b94ae0798f1da7ab346113
james daniel malvern <malvernator@fedora.access.network>
2016-10-07 14:58:23 +0100

    First commit

---
+++ src/pages/repos/repos.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-repos',
+  templateUrl: 'repos.html'
+})
+export class ReposPage {
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('Hello Repos Page');
+  }
+
+}
